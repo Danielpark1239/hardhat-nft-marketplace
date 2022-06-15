13ccb921d424af064c0a277953c08c9ccd5b8ba9
Daniel Park <Danielpark1239@gmail.com>
2022-06-15 18:34:44 -0400

    Added func to update frontend abi

--- deploy/99-update-frontend.js
+++ deploy/99-update-frontend.js
@@ -2,17 +2,33 @@ const { ethers, deployments, network } = require("hardhat")
 const fs = require("fs")
 
 const frontendContractsFile = "../nextjs-nft-marketplace-moralis/constants/networkMapping.json"
+const frontendAbiLocation = "../nextjs-nft-marketplace-moralis/constants/"
 
 module.exports = async function () {
     if (process.env.UPDATE_FRONTEND) {
         console.log("Updating frontend...")
+        await deployments.fixture(["nftmarketplace", "basicnft"])
         await updateContractAddresses()
+        await updateAbi()
         console.log("Frontend updated!")
     }
 }
 
+async function updateAbi() {
+    const nftMarketplace = await ethers.getContract("NftMarketplace")
+    fs.writeFileSync(
+        `${frontendAbiLocation}NftMarketplace.json`,
+        nftMarketplace.interface.format(ethers.utils.FormatTypes.json)
+    )
+
+    const basicNft = await ethers.getContract("BasicNFT")
+    fs.writeFileSync(
+        `${frontendAbiLocation}BasicNFT.json`,
+        basicNft.interface.format(ethers.utils.FormatTypes.json)
+    )
+}
+
 async function updateContractAddresses() {
-    await deployments.fixture("nftmarketplace")
     const nftMarketplace = await ethers.getContract("NftMarketplace")
     const chainId = network.config.chainId.toString()
     const contractAddresses = JSON.parse(fs.readFileSync(frontendContractsFile, "utf8"))
